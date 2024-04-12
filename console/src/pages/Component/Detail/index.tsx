c23fe4253389283da744a0dd83e2acaa45970ee4
tanglonghao <tanglonghao@bixin.cn>
2024-04-12 19:14:01 +0800

    首页、团队、404

--- console/src/pages/Component/Detail/index.tsx
+++ console/src/pages/Component/Detail/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import {PageContainer, ProDescriptions} from "@ant-design/pro-components";
+import {ModalForm, PageContainer, ProFormSelect, ProFormText, ProFormTextArea} from "@ant-design/pro-components";
 import useRequest from "@ahooksjs/use-request";
 import {ComponentService} from "@/services";
 import {useParams} from "@umijs/max";
@@ -7,12 +7,67 @@ import {Button, Space, Tag, Typography} from "antd";
 import ProCard from "@ant-design/pro-card";
 import {EditOutlined} from "@ant-design/icons";
 import Basic from "./components/Basic";
-import Tools from "@/pages/Component/Detail/components/Tools";
-import Activity from "@/pages/Component/Detail/components/Activity";
-import Scorecard from "@/pages/Component/Detail/components/Scorecard";
+import Tools from "./components/Tools";
+import Activity from "./components/Activity";
+import Scorecard from "./components/Scorecard";
+
+const EditBasicInfo = (props: API.Component) => {
+    return (
+        <ModalForm
+            width={500} title={"基本信息"}
+            trigger={<Button type={"text"} size={"small"} icon={<EditOutlined/>}/>}
+            initialValues={props}
+        >
+            <ProFormSelect name={"type"} label={"类型"}/>
+            <ProFormSelect name={"lifecycle"} label={"生命周期"}/>
+            <ProFormSelect name={"tier"} label={"层级"}/>
+            <ProFormSelect name={"team"} label={"团队"}/>
+            <ProFormSelect name={"repos"} label={"仓库"}/>
+            <ProFormSelect name={"chats"} label={"联系"}/>
+            <ProFormTextArea name={"annotation"} label={"注解"}/>
+        </ModalForm>
+    )
+}
+
+const Overview = (props: API.Component) => {
+    return (
+        <ProCard ghost gutter={[16, 16]} wrap>
+            <ProCard colSpan={12} title={"基础信息"} extra={<EditBasicInfo  {...props}/>}>
+                <Basic component={props}/>
+            </ProCard>
+            <ProCard colSpan={12} title={"工具"}
+                     extra={<Button type={"text"} size={"small"} icon={<EditOutlined/>}/>}>
+                <Tools component={props}/>
+            </ProCard>
+            <ProCard colSpan={24} title={"记分卡"}
+                     extra={<Button type={"link"} size={"small"}>查看更多</Button>}>
+                <Scorecard component={props}/>
+            </ProCard>
+            <ProCard colSpan={24} title={"最近变更"}
+                     extra={<Button type={"link"} size={"small"}>查看更多</Button>}>
+                <Activity component={props}/>
+            </ProCard>
+        </ProCard>
+    )
+}
+
+const EditMainInfo = (props: API.Component) => {
+    return (
+        <ModalForm
+            width={500} title={"主要信息"}
+            trigger={<Button>编辑</Button>}
+            initialValues={props}
+        >
+            <ProFormText name={"name"} label={"名称"}/>
+            <ProFormTextArea name={"description"} label={"描述"}/>
+            <ProFormSelect name={"tags"} label={"标签"} fieldProps={{mode: "tags"}}/>
+        </ModalForm>
+    )
+}
 
 const ComponentDetailPage: React.FC = () => {
     const params = useParams();
+    const [tabActive, setTabActive] = React.useState("overview")
     const {data, loading, run} = useRequest((id: string) => ComponentService.getComponent(id), {
         manual: true
     })
@@ -23,49 +78,33 @@ const ComponentDetailPage: React.FC = () => {
         }
     }, [params])
 
-    const content = (
-        <ProDescriptions/>
-    )
+    if (!data) {
+        return null;
+    }
 
     const tags = data?.tags?.map((tag) => <Tag color={"blue"}>{tag}</Tag>)
 
     const extra = (
         <Space>
-            <Button>编辑</Button>
+            <EditMainInfo {...data}/>
             <Button type={"link"} danger>下线</Button>
         </Space>
     )
 
     return (
-        <PageContainer title={data?.name} tags={tags}
-                       header={{style: {backgroundColor: "white"}}}
-                       content={<Typography.Text type={"secondary"}>{data?.description}</Typography.Text>}
-                       loading={loading} extra={extra}
-                       tabList={[
-                           {key: "overview", tab: "概要"},
-                           {key: "scorecard", tab: "记分卡"},
-                           {key: "dependencies", tab: "依赖组件"},
-                           {key: "activity", tab: "最近变更"},
-                       ]}
+        <PageContainer
+            title={data?.name} tags={tags}
+            header={{style: {backgroundColor: "white"}}}
+            content={<Typography.Text type={"secondary"}>{data?.description}</Typography.Text>}
+            loading={loading} extra={extra} tabActiveKey={tabActive} onTabChange={setTabActive}
+            tabList={[
+                {key: "overview", tab: "概要"},
+                {key: "scorecard", tab: "记分卡"},
+                {key: "dependencies", tab: "依赖组件"},
+                {key: "activity", tab: "最近变更"},
+            ]}
         >
-            <ProCard ghost gutter={[16, 16]} wrap>
-                <ProCard colSpan={12} title={"基础信息"}
-                         extra={<Button type={"text"} size={"small"} icon={<EditOutlined/>}/>}>
-                    {data && <Basic component={data}/>}
-                </ProCard>
-                <ProCard colSpan={12} title={"工具"}
-                         extra={<Button type={"text"} size={"small"} icon={<EditOutlined/>}/>}>
-                    {data && <Tools component={data}/>}
-                </ProCard>
-                <ProCard colSpan={24} title={"记分卡"}
-                         extra={<Button type={"link"} size={"small"}>查看更多</Button>}>
-                    {data && <Scorecard component={data}/>}
-                </ProCard>
-                <ProCard colSpan={24} title={"最近变更"}
-                         extra={<Button type={"link"} size={"small"}>查看更多</Button>}>
-                    {data && <Activity component={data}/>}
-                </ProCard>
-            </ProCard>
+            {tabActive === "overview" && <Overview {...data}/>}
         </PageContainer>
     )
 }
